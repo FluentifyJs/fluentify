9a5e49b398bf30ad543157d3e291fbef1a1d22df
Alexander Wennerstrøm <alexanderw0310@gmail.com>
2019-01-03 23:29:19 +0100

    Exposed completed and WIP components

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,33 @@
 import FBtn from './FBtn'
 import FRadio from './FRadio'
 import FRadioGroup from './FRadioGroup'
+import FCheckbox from './FCheckbox'
+import FSlider from './FSlider'
+import FSelect from './FSelect'
+import FAvatar from './FAvatar'
+import FInput from './FInput'
+import FAutocomplete from './FAutocomplete'
+import FList from './FList'
+import FListItem from './FListItem'
+import FListItemAvatar from './FListItemAvatar'
+import FListItemContent from './FListItemContent'
+import FMenu from './FMenu'
+import FProgressLinear from './FProgressLinear'
 
 export {
   FBtn,
   FRadio,
-  FRadioGroup
+  FRadioGroup,
+  FCheckbox,
+  FSlider,
+  FSelect,
+  FAvatar,
+  FInput,
+  FAutocomplete,
+  FList,
+  FListItem,
+  FListItemAvatar,
+  FListItemContent,
+  FMenu,
+  FProgressLinear
 }
